d839cd6acafba49f76b3d5021898a7f6490d88d0
ianMuchesia <ianmuchesia@yahoo.com>
2023-02-28 10:10:19 +0300

    deploy

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 require('dotenv').config()
 require('express-async-errors')
+
+//extra security packages
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+
+
+
 const express = require('express')
 
 const connectDB = require('./db/connect')
@@ -17,8 +26,18 @@ const jobRoute = require('./routes/jobs')
 const notFoundMiddleware = require('./middleware/not-found')
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
+app.set('trust proxy', 1)
+app.use(rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+}))
 
 app.use(express.json())
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
+
 
 //extra packages
 
